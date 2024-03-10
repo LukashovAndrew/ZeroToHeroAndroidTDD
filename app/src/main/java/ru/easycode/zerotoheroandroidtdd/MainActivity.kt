922a58b4251fc02e2ba689b489d5568c9ada4171
LukashovAndrew <Lukashov1986@gmail.com>
2024-03-10 12:58:56 +0300

    20240310 LA Task003Test Create View and textView without HTML

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,20 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val linearLayout = LinearLayout(this)
+        val textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.text = R.string.main_logo.toString()
+        linearLayout.addView(textView)
+
         setContentView(R.layout.activity_main)
     }
 }
