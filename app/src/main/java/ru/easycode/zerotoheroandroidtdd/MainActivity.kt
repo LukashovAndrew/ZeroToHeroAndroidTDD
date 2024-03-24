14734b2c9a9a4ecc343b341fffc5ab9c1331747a
LukashovAndrew <Lukashov1986@gmail.com>
2024-03-24 21:35:55 +0300

    20240319 LA Task007Test 2 solved without xml

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,17 +2,33 @@ package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.id = R.id.rootLayout
+
+        val textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.text = "Hello World"
+        linearLayout.addView(textView)
+
+        val button =  Button(this)
+        button.id = R.id.changeButton
+        button.text = "change"
+        linearLayout.addView(button)
+
+        setContentView(linearLayout)
 
         //val button = findViewById<Button>(R.id.changeButton)  - generic типизация
-        val button : Button = findViewById(R.id.changeButton)   //без джинерик типизации
-        val textView = findViewById<TextView>(R.id.titleTextView)
+
+
 
         button.setOnClickListener {
             textView.setText("I am an Android Developer!")
