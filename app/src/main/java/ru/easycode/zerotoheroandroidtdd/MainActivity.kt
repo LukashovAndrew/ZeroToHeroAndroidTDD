be912f451fb7c2d6a8d2b05696e32fcbe9988ea9
LukashovAndrew <Lukashov1986@gmail.com>
2024-03-24 23:39:19 +0300

    20240319 LA Task007Test 2 solved without xml saveState 2 methods

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,20 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var    textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button: Button = findViewById(R.id.changeButton)
-        val textView: TextView = findViewById(R.id.titleTextView)
+        textView = findViewById(R.id.titleTextView)
         button.setOnClickListener {
             textView.setText("I am an Android Developer!")
         }
 
+        savedInstanceState?.let {
+            textView.text = savedInstanceState.getString("key")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString("key", textView.text.toString())
+    }
+
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//        textView.text = savedInstanceState.getString("key")
+//    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
