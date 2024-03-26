ebd75292675bac1eef57df929bd0efbb8c30941c
LukashovAndrew <Lukashov1986@gmail.com>
2024-03-26 21:53:46 +0300

    20240326 LA Task008Test

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.hideButton)
+        val textView: TextView = findViewById(R.id.titleTextView)
+
+        button.setOnClickListener{
+            textView.visibility = View.INVISIBLE
+        }
     }
 
-    val button: Button = findViewById(R.id.hideButton)
-    val textView: TextView = findViewById(R.id.titleTextView)
+
 
 
 }
