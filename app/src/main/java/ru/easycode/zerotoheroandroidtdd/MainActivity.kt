0854bcc6f2710e7e5cb0b0bc114c1d42865a3819
LukashovAndrew <Lukashov1986@gmail.com>
2024-04-14 19:11:01 +0300

    20240326 LA Task008Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -8,19 +8,34 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var    textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button: Button = findViewById(R.id.hideButton)
-        val textView: TextView = findViewById(R.id.titleTextView)
+        textView = findViewById(R.id.titleTextView)
 
         button.setOnClickListener{
-            textView.visibility = View.INVISIBLE
+            textView.visibility = View.INVISIBLE //View.INVISIBLE
         }
+
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY, textView.visibility)
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.visibility = savedInstanceState.getInt(KEY)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 
-
+    companion object {
+        private const val  KEY = "visibilityKey"
+    }
 }
